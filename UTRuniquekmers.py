fd025c511cdabc7721e4d1b11976327309ca69e0
Matthew Taliaferro <matthew.taliaferro@ucdenver.edu>
2018-04-25 17:24:58 -0600

    Fixed UniqueSeq bug and added z-score to kmer script

--- UTRuniquekmers.py
+++ UTRuniquekmers.py
@@ -16,12 +16,17 @@ import sys
 from Bio import SeqIO
 import argparse
 import os
+import numpy as np
 
 
 #Count kmers in a single sequence
 def countkmers(seq, k):
 	#Get the total number of considered kmers.  This is different than len(seq) - k + 1 because we aren't considering every kmer (homopolymers, for example)
 	consideredkmers = 0
+
+	#If the length of the sequence is less than k, return None
+	if len(seq) < k:
+		return None, None
 
 	kmers = {} #{kmer : count}
 	seq = seq.replace('T', 'U').upper()
@@ -58,11 +63,15 @@ def countkmers(seq, k):
 #Count kmers in all seqs in the uniqueutrfasta
 def countkmers_fasta(fasta, k):
 	k = int(k)
+	seqcounter = 0
 
 	kmerdict = {} #{ENSGENE : {UTRnumber : {kmer : count}}}
 	consideredkmersdict = {} #{ENSGENE : {UTRnumber : total number of kmers}}
 	with open(fasta, 'r') as infh:
 		for record in SeqIO.parse(infh, 'fasta'):
+			seqcounter +=1
+			if seqcounter % 5000 == 0:
+				print('Counting kmers in sequence {0}...'.format(seqcounter))
 			genename = record.id.split('.')[0]
 			UTRnumber = int(record.id.split('uniqueUTR')[1])
 			kmers, consideredkmers = countkmers(str(record.seq), k)
@@ -77,6 +86,49 @@ def countkmers_fasta(fasta, k):
 
 	return kmerdict, consideredkmersdict
 
+def cleankmerdicts(kmerdict, consideredkmersdict):
+	#Sometimes a UTR is shorter than k, meaning that there are 0 considered kmers.
+	#This is a problem because we later divide by the number of considered kmers, which would be 0
+	#So we need to remove any UTR which has 'None' for kmers and/or consideredkmers
+	#We then need to change the UTR number of any UTR after that UTR in that gene.  If UTR2 is short,
+	#UTR3 becomes UTR2, UTR4 becomes UTR3, etc.
+	cleankmerdict = {}
+	cleanconsideredkmersdict = {}
+
+	for gene in kmerdict:
+		newgenedict_kmerdict = {} #{UTRnumber : {kmer : count}}}
+		newgenedict_consideredkmersdict = {} #{UTRnumber : total number of kmers}}
+		for UTRnumber in kmerdict[gene]:
+			oldnumberofutrs = len(kmerdict[gene])
+			#Remove this UTR if its value is None
+			#k = UTRnumber, v = kmerdict
+			newgenedict_kmerdict = {k : v for k, v in kmerdict[gene].items() if v}
+			#k = UTRnumber, v = totalnumberofkmers
+			newgenedict_consideredkmersdict = {k : v for k, v in consideredkmersdict[gene].items() if v}
+			newnumberofutrs = len(newgenedict_kmerdict)
+			if len(newgenedict_kmerdict) != len(newgenedict_consideredkmersdict):
+				print('ERROR!!! ', gene)
+
+		#If, after deleting all UTRs with None, there's only one left, delete the whole gene
+		if newnumberofutrs == 1:
+			continue
+		#If there's at least two left, reorder them and then make new dictionaries based on the reordering (3 --> 2, 2 --> 1, etc.)
+		elif newnumberofutrs >= 2:
+			reorderedgenedict_kmer = {} #{UTRnumber : {kmer : count}}}
+			reorderedgenedict_consideredkmers = {} # {UTRnumber : total number of kmers}}
+			i = 0
+			for kmerkey, consideredkmerkey in zip(sorted(newgenedict_kmerdict), sorted(newgenedict_consideredkmersdict)):
+				reorderedgenedict_kmer[i] = newgenedict_kmerdict[kmerkey]
+				reorderedgenedict_consideredkmers[i] = newgenedict_consideredkmersdict[consideredkmerkey]
+				i +=1
+
+			cleankmerdict[gene] = reorderedgenedict_kmer
+			cleanconsideredkmersdict[gene] = reorderedgenedict_consideredkmers
+
+	return cleankmerdict, cleanconsideredkmersdict
+
+
+
 #Calculate psi values for each kmer given a dictionary like kmerdict {ENSGENE: {UTRnumber : {kmers}}}
 def calculatepsi(kmerdict, consideredkmersdict, k):
 	#kmerdict = {} #{ENSGENE : {UTRnumber : {kmer : count}}}
@@ -90,13 +142,16 @@ def calculatepsi(kmerdict, consideredkmersdict, k):
 		utrcounts[gene] = len(kmerdict[gene])
 
 	#Get the raw kmer density for each kmer/UTR and the weighted kmer density for each kmer/UTR
+	genecounter = 0
 	for gene in kmerdict:
+		genecounter +=1
+		if genecounter % 1000 == 0:
+			print('Calculating weighted kmer densities for gene {0}...'.format(genecounter))
 		for utrnumber in kmerdict[gene]:
 			for kmer in kmerdict[gene][utrnumber]:
 				rawcount = kmerdict[gene][utrnumber][kmer]
 				weightingfactor = float(utrnumber / (utrcounts[gene] - 1)) #just like calculating psis in LABRAT
 				totalkmers = consideredkmersdict[gene][utrnumber]
-				print(gene, utrnumber)
 				rawdensity = rawcount / totalkmers
 				weightedkmerdensity = rawdensity * weightingfactor
 				
@@ -123,11 +178,42 @@ def calculatepsi(kmerdict, consideredkmersdict, k):
 			psi = 'NA'
 		psis[kmer] = psi
 
-	with open('/Users/mtaliaferro/Desktop/temp2.txt', 'w') as outfh:
-		for kmer in psis:
-			outfh.write(kmer + '\t' + str(psis[kmer]) + '\t' + str(kmer.count('G') + kmer.count('C')) + '\n')
-
 	return psis
+
+def normalizepsis(psis):
+	#We need to normalize psis based on their GC content.  This is done with a z-score normalization to all other kmers
+	#with the same GC count
+
+	#Get mean and sd for every GC count
+	GCdict = {} #{GC count : [psis]}
+	GCmeandict = {} #{GC count : meanpsi}
+	GCsddict = {} #{GC count : psi sd}
+	for kmer in psis:
+		GCcount = kmer.count('G') + kmer.count('C')
+		psi = psis[kmer]
+		if GCcount not in GCdict:
+			GCdict[GCcount] = [psi]
+		else:
+			GCdict[GCcount].append(psi)
+
+	for GCcount in GCdict:
+		psilist = GCdict[GCcount]
+		psimean = np.mean(psilist)
+		psisd = np.std(psilist)
+		GCmeandict[GCcount] = psimean
+		GCsddict[GCcount] = psisd
+
+	#Calculate z-scores
+	zs = {} #{kmer : zscore}
+	for kmer in psis:
+		psi = psis[kmer]
+		GCcount = kmer.count('G') + kmer.count('C')
+		psimean = GCmeandict[GCcount]
+		psisd = GCsddict[GCcount]
+		zscore = (psi - psimean) / psisd
+		zs[kmer] = zscore
+
+	return zs
 
 
 if __name__ == '__main__':
@@ -142,12 +228,14 @@ if __name__ == '__main__':
 	#If we aren't using a control
 	if not args.controlmode:
 		kmerdict, consideredkmersdict = countkmers_fasta(args.fasta, args.k)
+		kmerdict, consideredkmersdict = cleankmerdicts(kmerdict, consideredkmersdict)
 		psis = calculatepsi(kmerdict, consideredkmersdict, args.k)
+		zs = normalizepsis(psis)
 		#Write results
 		fn = args.fasta + '.utrkmers'
 		with open(fn, 'w') as outfh:
 			for kmer in psis:
-				outfh.write(kmer + '\t' + str(psis[kmer]) + '\n')
+				outfh.write(kmer + '\t' + str(psis[kmer]) + '\t' + str(kmer.count('G') + kmer.count('C')) + '\t' + str(kmer.count('CG')) + '\t' + str(zs[kmer]) + '\n')
 
 	#Genelists are list of ensembl IDs without dots
 	if args.controlmode == 'twogenelists':
