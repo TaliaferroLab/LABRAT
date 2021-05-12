10e16de41872ba754e1b7cf3bc4ae0c6a285303d
Matthew Taliaferro <taliaferrojm@gmail.com>
2021-05-12 15:15:25 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name = 'labrat',
+description = 'Lightweight Alignment Based Reckoning of Alternative Three-prime ends',
+author = 'Matthew Taliaferro',
+author_email = 'taliaferrojm@gmail.com',
+url = 'https://github.com/TaliaferroLab/LABRAT',
+version = '0.2.2'
+packages = ['labrat'],
+scripts = ['LABRAT.py', 'LABRAT_danRer.py', 'LABRAT_dm6annotation.py', 'LABRAT_rn6annotation.py'])
