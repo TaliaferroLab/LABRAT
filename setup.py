81745cff9f598774bc06dbf63de47691f47e281a
Matthew Taliaferro <taliaferrojm@gmail.com>
2021-05-12 15:46:41 -0600

    typo in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ description = 'Lightweight Alignment Based Reckoning of Alternative Three-prime 
 author = 'Matthew Taliaferro',
 author_email = 'taliaferrojm@gmail.com',
 url = 'https://github.com/TaliaferroLab/LABRAT',
-version = '0.2.2'
-packages = ['labrat'],
+version = '0.2.2',
 scripts = ['LABRAT.py', 'LABRAT_danRer.py', 'LABRAT_dm6annotation.py', 'LABRAT_rn6annotation.py'])
